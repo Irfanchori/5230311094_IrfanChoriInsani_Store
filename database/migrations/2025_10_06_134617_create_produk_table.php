8d513cd54215a3f062a2ba7ecc4cc00ae6614690
Irfanchori <irfanchori17@gmail.com>
2025-10-12 22:53:56 +0700

    first commit

---
+++ database/migrations/2025_10_06_134617_create_produk_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->increments('id_produk'); // otomatis jadi primary key + auto increment
+            $table->string('nama_produk', 100);
+            $table->string('kategori', 50);
+            $table->decimal('harga_satuan', 12, 2);
+            $table->integer('stok')->default(0);
+            $table->integer('satuan'); // 
+            $table->timestamps();
+
+            $table->unique('nama_produk', 'uq_produk_nama');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produk');
+    }
+};
